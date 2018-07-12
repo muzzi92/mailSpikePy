2c767fb260ab8e3fe3d7093457705ec6243e3f12
Muzzi Aldean <amiraldean@gmail.com>
2018-07-12 11:20:31 +0100

    Sends email via request  Mailgun API

--- mailer.py
+++ mailer.py
@@ -1,5 +1,6 @@
 from payment_service import PaymentService
 from nameko.events import event_handler
+import requests
 
 class Mailer(object):
 
@@ -7,8 +8,15 @@ class Mailer(object):
 
     @event_handler('payments', 'payment_received')
     def send(self, payload):
-        print(
-            f"""
+        key = 'b53481c39be1a2129486cde3809b6526-8b7bf2f1-7333ddf6'
+        sandbox = 'sandboxfec723b9368e438888acd3dde7fcee3e.mailgun.org'
+        recipient = 'amiraldean@gmail.com'
+        request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
+        requests.post(request_url, auth=('api', key), data={
+            'from': 'amiraldean@hotmail.com',
+            'to': recipient,
+            'subject': 'Hello',
+            'text': f"""
             Dear {payload['payee']['name']},
 
             You have received a payment of {payload['payment']['amount']} {payload['payment']['currency']} from {payload['client']['name']} ({payload['client']['email']}).
@@ -17,4 +25,5 @@ class Mailer(object):
 
             student.com
             """
-        )
+        })
+        print('Email Sent.')
